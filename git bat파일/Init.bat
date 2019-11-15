c6f83d181752c4b40b6a15b0c1e92a71120e03c3
terkiss <dl2000dl@naver.com>
2019-11-15 14:41:54 +0900

    자동화 등록

---
+++ git bat파일/Init.bat
@@ -0,0 +1,5 @@
+D:
+git clone "https://github.com./terkiss/peekaboo"
+
+git config --global user.email "너님 이메일 주소"
+git config --global user.Name "너님 계정 닉네임"
---
+++ git bat파일/commit2.bat
@@ -0,0 +1,5 @@
+D:
+cd %1
+git add *
+git commit -m %2
+git push origin master
